64252e2465f9ef0044fd4d065bc8f95b207dabf8
boforn lin <boforn@hotmail.com>
2012-02-11 15:16:22 +0800

    1. add close & write operation 2. add init_module()

--- cdata.c
+++ cdata.c
@@ -15,17 +15,43 @@
 #include <asm/io.h>
 #include <asm/uaccess.h>
 
+#define	MSG(m...)	printk(KERN_INFO "CDATA: " m "\n")
+#define	DEV_MAJOR	121
+#define	DEV_NAME	"debug"
+
 static int cdata_open(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+static int cdata_close(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+static int cdata_write(struct inode *inode, struct file *filp)
 {
 	return 0;
 }
 
 static struct file_operations cdata_fops = {	
 	open:		cdata_open,
+	release:	cdata_close,
+	write:		cdata_write,
 };
 
 int cdata_init_module(void)
 {
+	MSG("CDATA v0.1.0");
+	MSG("	Copyright (C) 2004 www.jollen.org");
+
+	if(register_chrdev(DEV_MAJOR, DEV_NAME, &cdata_fops) < 0)
+	{
+		MSG("Couldn't register a device.");
+		return -1;
+	}
+	
+	return 0;
 }
 
 void cdata_cleanup_module(void)
